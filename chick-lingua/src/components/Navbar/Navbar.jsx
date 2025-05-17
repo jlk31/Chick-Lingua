08888e4bf44c0d47c6e853fb224564412e262a1b
Joseph Kirkup <164064098+jlk31@users.noreply.github.com>
2025-05-17 16:03:01 +0000

    Styling changes

--- chick-lingua/src/components/Navbar/Navbar.jsx
+++ chick-lingua/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,9 @@ const Navbar = () => (
   <div className='nav'>
     <div className="nav-logo">Chick-Lingua</div>
     <ul className="nav-menu">
-      <li>Home</li>
+      <li>
+        <Link to="/" style={{ color: 'inherit', textDecoration: 'none' }}>Home</Link>
+      </li>
       <li>
         <Link to="/about" style={{ color: 'inherit', textDecoration: 'none' }}>About</Link>
       </li>
