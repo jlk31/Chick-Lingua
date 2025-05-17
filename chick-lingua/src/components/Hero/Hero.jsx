a20bb549307775c06c1a10835ba197a2ceccd064
Joseph Kirkup <164064098+jlk31@users.noreply.github.com>
2025-05-17 14:39:07 +0000

    Fixes for the hero component

--- chick-lingua/src/components/Hero/Hero.jsx
+++ chick-lingua/src/components/Hero/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = ({heroData, setHeroCount, heroCount, setPlayStatus, playStatus}) =>
         onClick={() => setHeroCount((prev) => (prev + 1) % 3)}
       />
       </div>
-      <div className="hero-play">
+      <div className="hero-dot-play">
         <ul className="hero-dots">
           <li onClick={()=>setHeroCount(0)} className={heroCount === 0?'hero-dot orange': 'hero-dot'}></li>
           <li onClick={()=>setHeroCount(1)} className={heroCount === 1?'hero-dot orange': 'hero-dot'}></li>
