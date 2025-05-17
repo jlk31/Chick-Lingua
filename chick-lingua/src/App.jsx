588f9f904a31b346c7dc3a79d95dadb3493ffb2d
Joseph Kirkup <164064098+jlk31@users.noreply.github.com>
2025-05-17 12:00:47 +0000

    Set up background .jsx component

--- chick-lingua/src/App.jsx
+++ chick-lingua/src/App.jsx
@@ -1,8 +1,14 @@
-
 const App = () => {
+  let heroData = [
+    {text1:"Master German", text2:"the fun way"},
+    {text1:"Turn German", text2:"into a game"},
+    {text1:"Language learning", text2:"that doesnt feel like homework"},
+  ]
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      
+      <Background playStatus={playStatus} heroCount={heroCount}/>
     </div>
   )
 }
